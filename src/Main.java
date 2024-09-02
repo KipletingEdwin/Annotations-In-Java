247f8849b8d8fc36f15ae73985d762a6b3e2116e
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-09-02 22:49:30 +0300

    check if isAnnotationPresent is present

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
 
 //        @SuppressWarnings("unused")
         Cat myCat = new Cat("Stella");
@@ -10,6 +13,12 @@ public class Main {
             System.out.println("This thing is very important");
         } else {
             System.out.println("This thing is not very important");
+        }
+
+        for(Method method : myCat.getClass().getDeclaredMethods()){
+            if(method.isAnnotationPresent(RunImmediately.class)){
+                method.invoke(myCat);
+            }
         }
 
     }
